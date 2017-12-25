6a46150c14a4537edb407b6acb1d073ae2cb95fb
Pontus Wirsching <pontus.wirsching@sundbyberg.it-gymnasiet.se>
2017-12-25 23:58:55 +0100

    Fixed Keyboard bug on OSx

--- src/com/spel/graphics/Window.java
+++ src/com/spel/graphics/Window.java
@@ -1,5 +1,9 @@
 package com.spel.graphics;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -8,33 +12,52 @@ import com.spel.util.input.Keyboard;
 public class Window {
 
 	private JFrame frame;
-	
+
 	public Window(String title, int width, int height) {
-		
+
+		if (System.getProperty("os.name").equals("Mac OS X"))
+		try {
+			Runtime.getRuntime().exec("defaults write -g ApplePressAndHoldEnabled -bool false");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		frame = new JFrame(title);
 		frame.setSize(width, height);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				if (System.getProperty("os.name").equals("Mac OS X"))
+				try {
+					Runtime.getRuntime().exec("defaults write -g ApplePressAndHoldEnabled -bool true");
+				} catch (IOException ee) {
+					ee.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
 		frame.setFocusable(true);
-		
+
 		frame.addKeyListener(new Keyboard());
-		
+
 		frame.setVisible(true);
 
 	}
-	
+
 	public void setPanel(JPanel panel) {
 		frame.add(panel);
 		frame.setVisible(true);
 	}
-	
+
 	public int getWidth() {
 		return frame.getWidth();
 	}
-	
+
 	public int getHeight() {
 		return frame.getHeight();
 	}
-	
+
 }
